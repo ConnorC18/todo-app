e3e4f0cb6354cc6d7b63fe6c192bd3f4b40512d0
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2024-02-02 15:11:41 +0200

    navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <header className="shadow-sm">
+      <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
+        <Link href="/" className="text-xl font-bold tracking-tight">
+          Todo App
+        </Link>
+        <Button asChild>
+          <Link href="/">Make a ToDo</Link>
+        </Button>
+      </nav>
+    </header>
+  );
+}
