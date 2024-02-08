c9dbfd78727b5c5b7ae48e5a7ac421526b04b152
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2024-02-08 14:10:56 +0200

    add LogIn and LogOut buttons in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,16 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { auth, signOut } from "@/auth";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await auth();
+
+  const serverAction = async () => {
+    "use server";
+
+    await signOut({ redirectTo: "/", redirect: true });
+  };
+
   return (
     <header className="shadow-sm">
       <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
@@ -9,11 +18,17 @@ export default function Navbar() {
           Todo App
         </Link>
         <div className="flex gap-6">
+          {session?.user ? (
+            <form action={serverAction}>
+              <Button variant="ghost">Log Out</Button>
+            </form>
+          ) : (
+            <Button asChild variant="outline">
+              <Link href="/auth/login">Log In</Link>
+            </Button>
+          )}
           <Button asChild>
             <Link href="/todo/new">Make a ToDo</Link>
-          </Button>
-          <Button asChild>
-            <Link href="/auth/login">Log In</Link>
           </Button>
         </div>
       </nav>
